aa9f824fc432553d6910b07d001e70cbf3d5a93e
Rabin Chakraborty <rabin747809@gmail.com>
2023-04-17 01:38:06 +0530

    Added logic too start the game and some major bug fixes

--- Simon Game/game.js
+++ Simon Game/game.js
@@ -1,8 +1,11 @@
 let buttonColours = ['red', 'blue', 'green', 'yellow'];
 let gamePattern = [];
 let userClickedPattern = [];
-
+let started = false;
+let level = 0;
 function nextSequence() {
+  level++;
+  $('#level-title').text('level ' + level);
   let num = Math.floor(Math.random() * 4);
   let randomChosenColour = buttonColours[num];
   gamePattern.push(randomChosenColour);
@@ -10,8 +13,7 @@ function nextSequence() {
     .fadeIn(100)
     .fadeOut(100)
     .fadeIn(100);
-  var audio = new Audio('sounds/' + randomChosenColour + '.mp3');
-  audio.play();
+  playSound(randomChosenColour);
 }
 
 function animatePress(currentColour) {
@@ -26,8 +28,17 @@ $('.btn').on('click', function () {
   userClickedPattern.push(userChosenColour);
   console.log(userClickedPattern);
   playSound(userChosenColour);
+  animatePress(userChosenColour);
 });
 function playSound(name) {
   var audio = new Audio('sounds/' + name + '.mp3');
   audio.play();
 }
+
+$(document).keydown(function () {
+  if (!started) {
+    $('#level-title').text('level ' + level);
+    nextSequence();
+    started = true;
+  }
+});
