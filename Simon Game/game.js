2e5f13bb2e69ff79b31873efaad2ceb9960e474b
Rabin Chakraborty <rabin747809@gmail.com>
2023-04-17 02:07:13 +0530

    added check to the user answers so the game can level up

--- Simon Game/game.js
+++ Simon Game/game.js
@@ -4,6 +4,7 @@ let userClickedPattern = [];
 let started = false;
 let level = 0;
 function nextSequence() {
+  userClickedPattern = [];
   level++;
   $('#level-title').text('level ' + level);
   let num = Math.floor(Math.random() * 4);
@@ -22,6 +23,22 @@ function animatePress(currentColour) {
     $('#' + currentColour).removeClass('pressed');
   }, 100);
 }
+function playSound(name) {
+  var audio = new Audio('sounds/' + name + '.mp3');
+  audio.play();
+}
+function checkAnswer(currentLevel) {
+  if (gamePattern[currentLevel] === userClickedPattern[currentLevel]) {
+    console.log('success');
+    if (userClickedPattern.length === gamePattern.length) {
+      setTimeout(function () {
+        nextSequence();
+      }, 1000);
+    }
+  } else {
+    console.log('wrong');
+  }
+}
 
 $('.btn').on('click', function () {
   let userChosenColour = this.id;
@@ -29,11 +46,8 @@ $('.btn').on('click', function () {
   console.log(userClickedPattern);
   playSound(userChosenColour);
   animatePress(userChosenColour);
+  checkAnswer(userClickedPattern.length - 1);
 });
-function playSound(name) {
-  var audio = new Audio('sounds/' + name + '.mp3');
-  audio.play();
-}
 
 $(document).keydown(function () {
   if (!started) {
